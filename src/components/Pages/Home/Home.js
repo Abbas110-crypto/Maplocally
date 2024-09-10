422b2a895d170d5c5cf63b518b82b423cebd36ad
Abbas Mehdi <abbasmehdi692@gmail.com>
2024-09-11 02:00:50 +0500

    maplocally homepage herosection incomplete

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from 'react';
+import './Home.css';
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='main'>
+      <div className='herosection'>
+        <h1 className='heading'>Discover dreams in New York's Iconic Spots</h1>
+        <p className='paragraph'>like the Empire State Buliding, Ouggenheim Museum, ond Michelin-starred Jean Georges</p>
+      </div>
+    </div>
   )
 }
 
