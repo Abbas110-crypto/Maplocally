5ca075d7e169aff15794f44f97cbfd2ba3a18858
SyedKumailAbbas <k214834@nu.edu.pk>
2024-10-27 02:54:31 +0500

    apos error resolved

--- src/app/PostDetail/page.jsx
+++ src/app/PostDetail/page.jsx
@@ -12,9 +12,9 @@ const PostDetail = () => {
     const placeData = {
         title: "Brief about the place",
         sections: [
-            "PostDetail your journey begins, you’ll be greeted by your experienced guide, who will share fascinating insights about Iceland’s natural wonders and cultural heritage. The adventure kicks off with a scenic drive along the iconic Ring Road, where you’ll witness breathtaking landscapes that range from towering glaciers to black sand beaches.",
-            "Your first stop is the mesmerizing Seljalandsfoss waterfall, where you’ll have the unique opportunity to walk behind the cascading water, feeling the cool mist on your face. Next, you’ll visit the powerful Skógafoss waterfall, one of Iceland’s largest, where rainbows often form in the mist.",
-            "After soaking in the beauty of these natural wonders, the tour continues to the majestic Sólheimajökull glacier. Here, you’ll embark on an unforgettable journey into a hidden ice cave, a rare and magical experience. The intricate ice formations and vibrant blue hues within the cave create a surreal atmosphere, perfect for capturing stunning photographs."
+            "PostDetail your journey begins, you will be greeted by your experienced guide, who will share fascinating insights about Iceland is natural wonders and cultural heritage. The adventure kicks off with a scenic drive along the iconic Ring Road, where you will witness breathtaking landscapes that range from towering glaciers to black sand beaches.",
+            "Your first stop is the mesmerizing Seljalandsfoss waterfall, where you will have the unique opportunity to walk behind the cascading water, feeling the cool mist on your face. Next, you will visit the powerful Skógafoss waterfall, one of Iceland wis largest, where rainbows often form in the mist.",
+            "After soaking in the beauty of these natural wonders, the tour continues to the majestic Sólheimajökull glacier. Here, you will embark on an unforgettable journey into a hidden ice cave, a rare and magical experience. The intricate ice formations and vibrant blue hues within the cave create a surreal atmosphere, perfect for capturing stunning photographs."
         ]
     };
     return (
